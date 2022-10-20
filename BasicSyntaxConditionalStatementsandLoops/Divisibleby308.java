5613dc77bb8ecac8161f50904f9bc7f0a7eb3897
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-10-21 01:51:17 +0300

    Create Divisibleby308.java

---
+++ BasicSyntaxConditionalStatementsandLoops/Divisibleby308.java
@@ -0,0 +1,11 @@
+package BasicSyntaxConditionalStatementsandLoops;
+
+public class Divisibleby308 {
+    public static void main(String[] args) {
+
+
+        for (int i = 3; i <=100 ; i+=3) {
+            System.out.println(i);
+        }
+    }
+}
