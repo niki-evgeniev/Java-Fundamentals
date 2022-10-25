57d7fc99328a5072d07cdef2cbdb45ae5e96d226
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-10-26 00:32:22 +0300

    Create MultiplicationTable2_11.java

---
+++ BasicSyntaxConditionalStatementsandLoops/MultiplicationTable2_11.java
@@ -0,0 +1,19 @@
+package BasicSyntaxConditionalStatementsandLoops;
+
+import java.util.Scanner;
+
+public class MultiplicationTable2_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        int number2 = Integer.parseInt(scanner.nextLine());
+        for (int i = number2; i <= 10; i++) {
+            int sum = number * i;
+            System.out.printf("%d X %d = %d%n", number, i, sum);
+        }
+        if (number2 > 10) {
+            int sum2 = number2 * number;
+            System.out.printf("%d X %d = %d", number, number2, sum2);
+        }
+    }
+}
