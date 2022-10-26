293904c710d5870127d74895ed47162688429a89
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-10-27 01:50:16 +0300

    Create test.java

---
+++ BasicSyntaxConditionalStatementsandLoops/test.java
@@ -0,0 +1,10 @@
+package BasicSyntaxConditionalStatementsandLoops;
+
+public class test {
+    public static void main(String[] args) {
+        int n = 2543;
+        int s = n % 10;
+        System.out.println(s);
+    }
+
+}
