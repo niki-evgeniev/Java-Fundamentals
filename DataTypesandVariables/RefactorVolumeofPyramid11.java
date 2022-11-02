1d791964180c79933499fe081fc58306144d745e
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-03 01:35:02 +0200

    Create RefactorVolumeofPyramid11.java

---
+++ DataTypesandVariables/RefactorVolumeofPyramid11.java
@@ -0,0 +1,17 @@
+package DataTypesandVariables;
+
+import java.util.Scanner;
+
+public class RefactorVolumeofPyramid11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double length = Double.parseDouble(scanner.nextLine());
+        double width = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+        double volume = (length * width * height) / 3;
+        System.out.print("Length: ");
+        System.out.print("Width: ");
+        System.out.print("Height: ");
+        System.out.printf("Pyramid Volume: %.2f", volume);
+    }
+}
