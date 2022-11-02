520240c60edad87333300dda73134d5403296cad
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-03 01:35:15 +0200

    Create TownInfo04.java

---
+++ DataTypesandVariables/TownInfo04.java
@@ -0,0 +1,13 @@
+package DataTypesandVariables;
+
+import java.util.Scanner;
+
+public class TownInfo04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String city = scanner.nextLine();
+        int population = Integer.parseInt(scanner.nextLine());
+        short area = Short.parseShort(scanner.nextLine());
+        System.out.printf("Town %s has population of %d and area %d square km.",city,population,area);
+    }
+}
