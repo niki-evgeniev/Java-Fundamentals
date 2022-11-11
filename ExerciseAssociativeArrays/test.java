92b05e1695b91b42715937be2a4e296faa676f1c
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-12 01:43:15 +0200

    Create test.java

---
+++ ExerciseAssociativeArrays/test.java
@@ -0,0 +1,15 @@
+package ExerciseAssociativeArrays;
+
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        String text = "";
+        for (int i = 0; i < 1000000; i++)
+            text += "a";
+        System.out.println(new Date());
+
+    }
+
+}
