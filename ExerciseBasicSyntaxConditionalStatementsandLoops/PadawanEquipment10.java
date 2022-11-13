c08b36aa9ce1c2e4fcbf03eb3f7b4413fce1da2b
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-13 02:28:16 +0200

    Create PadawanEquipment10.java

---
+++ ExerciseBasicSyntaxConditionalStatementsandLoops/PadawanEquipment10.java
@@ -0,0 +1,26 @@
+package ExerciseBasicSyntaxConditionalStatementsandLoops;
+
+import java.util.Scanner;
+
+public class PadawanEquipment10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double money = Double.parseDouble(scanner.nextLine());
+        int students = Integer.parseInt(scanner.nextLine());
+        double priceLights = Double.parseDouble(scanner.nextLine());
+        double priceRobes = Double.parseDouble(scanner.nextLine());
+        double priceBelts = Double.parseDouble(scanner.nextLine());
+        double studentsCount = students;
+        int freebelts = 0;
+        if (students > 5) {
+            freebelts = students / 6;
+            studentsCount -= freebelts;
+        }
+        double totalPrice = priceLights * Math.ceil(students + students * 0.1) + priceRobes * students + priceBelts * studentsCount;
+        if (money >= totalPrice) {
+            System.out.printf("The money is enough - it would cost %.2flv.", totalPrice);
+        } else {
+            System.out.printf("George Lucas will need %.2flv more.",totalPrice - money);
+        }
+    }
+}
