b6f5849a303dafeba749ecdf81872ee53b59ad43
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-20 00:37:13 +0200

    Create IntegerOperations01.java

---
+++ ExerciseDataTypesandVariables/IntegerOperations01.java
@@ -0,0 +1,16 @@
+package ExerciseDataTypesandVariables;
+
+import java.util.Scanner;
+
+public class IntegerOperations01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
+        int d = Integer.parseInt(scanner.nextLine());
+
+        int results = ( a + b ) / c * d;
+        System.out.println(results);
+    }
+}
