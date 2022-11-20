d45ae65105ea2e83ffdd91912047c6dfa949b3be
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-21 01:12:11 +0200

    Create SumDigits02.java

---
+++ ExerciseDataTypesandVariables/SumDigits02.java
@@ -0,0 +1,17 @@
+package ExerciseDataTypesandVariables;
+
+import java.util.Scanner;
+
+public class SumDigits02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        while (n > 0){
+            int number = n % 10;
+            sum += number;
+            n = n / 10;
+        }
+        System.out.println(sum);
+    }
+}
