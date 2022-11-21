996921ade42223326287788e0bb0f7286f442633
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-22 00:03:43 +0200

    Create WaterOverflow07.java

---
+++ ExerciseDataTypesandVariables/WaterOverflow07.java
@@ -0,0 +1,23 @@
+package ExerciseDataTypesandVariables;
+
+import java.util.Scanner;
+
+public class WaterOverflow07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int tank = 255;
+        for (int i = 0; i < n; i++) {
+
+            int litters = Integer.parseInt(scanner.nextLine());
+            if (litters > tank){
+                System.out.println("Insufficient capacity!");
+            }else {
+                tank -= litters;
+            }
+        }
+        int total = 255 - tank;
+        System.out.println(total);
+
+    }
+}
