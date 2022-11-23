f36bd33027714d84091bde3ab6cc211f7ee564e7
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-24 01:24:45 +0200

    Create ArrayRotation04.java

---
+++ ExerciseArrays/ArrayRotation04.java
@@ -0,0 +1,24 @@
+package ExerciseArrays;
+
+import java.util.Scanner;
+
+public class ArrayRotation04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arr = scanner.nextLine().split(" ");
+        int rotations = Integer.parseInt(scanner.nextLine());
+        String savefirstNum = " ";
+
+        for (int i = 1; i <= rotations; i++) {
+            savefirstNum = arr[0];
+            for (int j = 0; j < arr.length - 1; j++) {
+                arr[j] = arr[j + 1];
+
+            }
+            arr[arr.length - 1] = savefirstNum;
+        }
+        for (String number : arr) {
+            System.out.print(number + " ");
+        }
+    }
+}
