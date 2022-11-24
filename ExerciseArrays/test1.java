70efe436ce58f19006af292c6c133e9dd65f4627
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-11-25 01:51:28 +0200

    Create test1.java

---
+++ ExerciseArrays/test1.java
@@ -0,0 +1,13 @@
+package ExerciseArrays;
+
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        String gfg1 = String.join(" < ", "Four", "Five", "Six", "Seven");
+
+        System.out.println(gfg1);
+    }
+
+    }
+
