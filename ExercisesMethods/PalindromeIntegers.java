f70488d1b23d43ee11ffe88ab55f6dac11aa98b0
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-12-22 00:05:02 +0200

    Create PalindromeIntegers.java

---
+++ ExercisesMethods/PalindromeIntegers.java
@@ -0,0 +1,40 @@
+package ExercisesMethods;
+
+import java.util.Scanner;
+
+public class PalindromeIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String console = scanner.nextLine();
+        while (!console.equals("END")) {
+            int n = Integer.parseInt(console);
+            palindromeIntegers(n);
+            console = scanner.nextLine();
+        }
+    }
+
+    public static void palindromeIntegers(int n) {
+        int firstNumber = 0;
+        int secondNumber = 0;
+        int secN = n;
+        //взимаме 1вото число
+        while (n > 0) {
+            firstNumber = n % 10;
+            n = n / 10;
+//            if (9 >= n) {
+//                break;
+//            }
+        }
+        for (int i = 0; i < 1; i++) {
+            secondNumber = secN % 10;
+
+
+            if (firstNumber == secondNumber) {
+               System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+        }
+    }
+}
