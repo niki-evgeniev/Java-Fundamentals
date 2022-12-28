8db36506fa4137a52c541c2a869424ce55171aad
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-12-28 02:13:22 +0200

    Create Calculations04.java

---
+++ Methods/Calculations04.java
@@ -0,0 +1,48 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Calculations04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = scanner.nextLine();
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        switch (command) {
+            case "add":
+                add(a, b);
+                break;
+            case "multiply":
+                multiply(a, b);
+                break;
+            case "subtract":
+                subtract(a, b);
+                break;
+            case "divide":
+                divide(a, b);
+                break;
+        }
+    }
+
+    public static void add(int a, int b) {
+        int results = a + b;
+        System.out.println(results);
+    }
+
+    public static void multiply(int a, int b) {
+        int results = a * b;
+        System.out.println(results);
+    }
+
+    public static void subtract(int a, int b) {
+        int results = a - b;
+        System.out.println(results);
+    }
+
+    public static void divide(int a, int b) {
+        int results = a / b;
+        System.out.println(results);
+    }
+}
