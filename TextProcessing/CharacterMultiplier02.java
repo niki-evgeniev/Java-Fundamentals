893a153de012e8901e948e89d0a2e156f03aac29
Nikolay Ivanov <niki.evgeniev@gmail.com>
2022-12-30 02:25:53 +0200

    Create CharacterMultiplier02.java

---
+++ TextProcessing/CharacterMultiplier02.java
@@ -0,0 +1,9 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class CharacterMultiplier02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
